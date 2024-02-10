7751fd3118210e99bc7a979e44674603793fbaa8
Martin Flischman <hem0inn@icloud.com>
2024-02-10 18:42:23 +0200

    Initial commit
    feat: add html, css and javascript files including, content, styling and functionality for the kick sound button
    
    feat: add app sound files

---
+++ scrip.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Get reference to the kick button
+    const kickButton = document.getElementById('kick-button')
+    
+    // Get reference to the kick sound
+    const kickSound = document.getElementById('kick-sound')
+    
+    // When the kick button is clicked, play the kick sound
+    kickButton.addEventListener('click', function () {
+        kickSound.currentTime = 0 // Rewind sound to the beginning to allow for rapid playback
+        kickSound.play()
+    })
+})
