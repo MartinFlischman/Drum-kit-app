3136dc10f9c02438903d7722171c4e0044be346f
Martin Flischman <hem0inn@icloud.com>
2024-02-11 16:20:05 +0200

    Ride sound
    feat: add button and audio for the ride sound to the html
    
    feat: cmake the ride button play the ride sound when clicked by connecting it in the javascript by adding an event listener

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-// Retrieve the kick button element
+// Retrieve the button elements
 const kickButton = document.querySelector("#kick");
+const rideButton = document.querySelector("#ride");
 
-// Retrieve the kick sound element
+// Retrieve the sound elements
 const kickSound = document.querySelector("#kick-sound");
+const rideSound = document.querySelector("#ride-sound");
 
 // Attach a click event listener to the kick button
 kickButton.addEventListener("click", () => {
@@ -11,4 +13,12 @@ kickButton.addEventListener("click", () => {
     
     // Play the kick sound
     kickSound.play();
+});
+
+rideButton.addEventListener("click", () => {
+    // Reset the sound time to replay it
+    rideSound.currentTime = 0;
+
+    // Play the ride sound
+    rideSound.play();
 });
